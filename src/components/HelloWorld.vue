a6b43ec01d4104b6c3018fa86577447dfede7e33
Miny <944307891@qq.com>
2022-05-10 21:01:28 +0800

    feat: 加入less,vue-router,pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <img src="@/assets/logo.png" alt="1" />
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <div>pinia:{{ AppStore.counter }}</div>
+    <div>pinia:{{ AppStore.doubleCount }}</div>
+    <button type="button" @click="AppStore.increment()">pinia-button</button>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
+
+import { useAppStoreWithOut } from '@/store/app'
+const AppStore = useAppStoreWithOut()
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-</template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+<style scoped lang="less">
+.hello {
+  h1 {
+    color: aqua;
+  }
 }
 </style>
---
+++ src/views/main/Main.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>主页面</div>
+  <hello-world msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<style lang="less" scoped></style>
